ed9a17fa977fbf01ddc499b1ac338875a55dbd13
SerjLeo <sergejleontev111@gmail.com>
2021-11-07 22:51:28 +0300

    +

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+// const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const ImageminPlugin = require("imagemin-webpack");
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
@@ -19,13 +19,6 @@ const plugins = () => {
                 collapseWhitespace: isProd
             }
         }),
-        // new FaviconsWebpackPlugin({
-        //     logo: './logo.png',
-        //     prefix: 'assets/favicon/',
-        //     emitStats: false,
-        //     cache: true,
-        //     inject: true
-        // }),
         new MiniCssExtractPlugin({
             filename: '[name].[hash].css'
         }),
@@ -40,25 +33,25 @@ const plugins = () => {
         })
     ]
 
-    // if (isProd){
-    //     // base.push(new BundleAnalyzerPlugin())
-    //     base.push(new ImageminPlugin({
-    //         bail: false, // Ignore errors on corrupted images
-    //         cache: true,
-    //         imageminOptions: {
-    //           // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
-    //           // Lossless optimization with custom option
-    //           // Feel free to experiment with options for better result for you
-    //           plugins: [
-    //             ["mozjpeg", {
-    //                 progressive: true,
-    //                 quality: 50
-    //             }],
-    //             ["optipng", { optimizationLevel: 5 }],
-    //           ]
-    //         }
-    //       }))
-    // }
+    if (isProd){
+        // base.push(new BundleAnalyzerPlugin())
+        base.push(new ImageminPlugin({
+            bail: false, // Ignore errors on corrupted images
+            cache: true,
+            imageminOptions: {
+              // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+              // Lossless optimization with custom option
+              // Feel free to experiment with options for better result for you
+              plugins: [
+                ["mozjpeg", {
+                    progressive: true,
+                    quality: 50
+                }],
+                ["optipng", { optimizationLevel: 5 }],
+              ]
+            }
+          }))
+    }
 
     return base
 }
@@ -126,49 +119,21 @@ module.exports = {
                         reloadAll: true
                     }
                   },
-                  // Translates CSS into CommonJS
                   'css-loader',
-                  // Compiles Sass to CSS
                   'sass-loader',
                 ],
             },
             {
                 test:/\.(png|jpg|svg|jpeg)$/,
-                use: [
-                    'file-loader',
-                    // {
-                    //     loader: 'image-webpack-loader',
-                    //     options: {
-                    //         mozjpeg: {
-                    //             progressive: true,
-                    //             quality: 50
-                    //           },
-                    //           // optipng.enabled: false will disable optipng
-                    //           optipng: {
-                    //             enabled: false,
-                    //           },
-                    //           pngquant: {
-                    //             quality: [0.65, 0.90],
-                    //             speed: 4
-                    //           },
-                    //           gifsicle: {
-                    //             interlaced: false,
-                    //           },
-                    //           // the webp option will enable WEBP
-                    //           webp: {
-                    //             quality: 75
-                    //           }
-                    //     },
-                    // }
-                ]
+                loader: 'file-loader'
             },
             {
                 test:/\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
+                loader: 'file-loader'
             },
             {
                 test:/\.csv$/,
-                use: ['csv-loader']
+                loader: 'file-loader'
             },
             {
                 test: /\.js$/,
